2971a4846134277956e7d17c30eb47ce1eee511d
Robert Fydrych <fydrych.robert.w@gmail.com>
2023-02-19 21:52:47 +0000

    remove tasks

--- js/script.js
+++ js/script.js
@@ -12,6 +12,19 @@ console.log('Hello World!');
         },
     ];
 
+    const addNewTask = (newTaskContent) => {
+        tasks.push({
+            content: newTaskContent,
+        });
+
+        render();
+    };
+
+    const removeTask = (taskIndex) => {
+        tasks.splice(taskIndex, 1);
+        render();
+    }
+
     const render = () => {
         let htmlString = "";
 
@@ -20,21 +33,25 @@ console.log('Hello World!');
                 <li
                 ${task.done ? " style=\"text-decoration: line-through\"" : ""} 
                 >
+                <button class="js-remove">delete</button>
                     ${task.content}
                 </li>
             `;
         }
 
         document.querySelector(".js-tasks").innerHTML = htmlString;
-    };
 
-    const addNewTask = (newTaskContent) => {
-        tasks.push({
-            content: newTaskContent,
+        const removeButtons = document.querySelectorAll(".js-remove");
+
+        removeButtons.forEach((removeButton, index) => {
+            removeButton.addEventListener("click", () => {
+                removeTask(index);
+            });
         });
-
-        render();
     };
+
+
+
 
     const onFormSubmit = (event) => {
         event.preventDefault();
